a1be0e848ea3093a1b22cf4ff307a3898a2fe355
attoyibi <43211768+attoyibi@users.noreply.github.com>
2023-11-21 16:31:32 +0700

    vue 3 component basic usage

---
+++ vue 3 component/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Pesan Dari App.vue" />
+    <ComponentProps
+      message="Halo, ini adalah contoh component"
+      count="10"
+      isFinished="true"
+      :address="{
+        street: 'Jl. Raya Geger',
+        city: 'Desa Geger',
+        province: 'Jawa Timur',
+      }"
+      :list="['satu', 'dua', 'tiga']"
+    />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import ComponentProps from "./components/ComponentProps.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    ComponentProps,
+  },
+};
+</script>
---
+++ vue 3 component/src/components/ComponentProps.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="component">
+    <h1>{{ message }}</h1>
+    <p>Jumlah hitungan: {{ count }}</p>
+    <p v-if="isFinished">Selesai</p>
+    <p v-else>Belum selesai</p>
+    <p>Alamat:</p>
+    <ul>
+      <li>Jalan: {{ address.street }}</li>
+      <li>Kota: {{ address.city }}</li>
+      <li>Provinsi: {{ address.province }}</li>
+    </ul>
+    <p>List:</p>
+    <ol>
+      <li v-for="item in list" :key="item">{{ item }}</li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ComponentProps",
+  props: {
+    message: {
+      type: String,
+      required: true,
+    },
+    count: {
+      type: Number,
+      default: 0,
+    },
+    isFinished: {
+      type: Boolean,
+      default: false,
+    },
+    address: {
+      type: Object,
+      default: () => ({
+        street: "",
+        city: "",
+        province: "",
+      }),
+    },
+    list: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
