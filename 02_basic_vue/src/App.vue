58a226f2e0c4c41ab22f86757236224c44a2ce2b
attoyibi <43211768+attoyibi@users.noreply.github.com>
2025-03-11 15:04:39 +0700

    initial project

---
+++ 02_basic_vue/src/App.vue
@@ -0,0 +1,8 @@
+<template>
+<h1> Halo ini App </h1>
+<template /> 
+
+<script setup>
+import Navbar from '@'
+
+</script>
--- vue 3 component/src/App.vue
+++ vue 3 component/src/App.vue
@@ -1,29 +1,21 @@
 <template>
   <div id="app">
     <HelloWorld msg="Pesan Dari App.vue" />
-    <ComponentProps
-      message="Halo, ini adalah contoh component"
-      count="10"
-      isFinished="true"
-      :address="{
-        street: 'Jl. Raya Geger',
-        city: 'Desa Geger',
-        province: 'Jawa Timur',
-      }"
-      :list="['satu', 'dua', 'tiga']"
-    />
-  </div>
+    <Home />
+</div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
 import ComponentProps from "./components/ComponentProps.vue";
+import Home from "./view/Home.vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
-    ComponentProps,
+    ComponentProps, 
+    Home
   },
 };
 </script>
